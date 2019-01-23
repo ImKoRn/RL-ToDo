75eb19fb2c575318e5fb0b7ca9161a0ff9a21795
Dmytro Korniichuk <dkorniichuk@c.snap.com>
2019-01-23 17:29:41 +0200

    Migration to androidx

--- app/src/main/java/com/richelieu/myawesometodo/login/LoginActivity.kt
+++ app/src/main/java/com/richelieu/myawesometodo/login/LoginActivity.kt
@@ -1,7 +1,7 @@
 package com.richelieu.myawesometodo.login
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.richelieu.myawesometodo.R
 import com.richelieu.myawesometodo.manager.ToDoFragmentManager
 
--- app/src/main/java/com/richelieu/myawesometodo/manager/ToDoFragmentManager.kt
+++ app/src/main/java/com/richelieu/myawesometodo/manager/ToDoFragmentManager.kt
@@ -1,7 +1,7 @@
 package com.richelieu.myawesometodo.manager
 
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.richelieu.myawesometodo.R
 import com.richelieu.myawesometodo.login.LoginFragment
 import com.richelieu.myawesometodo.login.LoginSelectionFragment
